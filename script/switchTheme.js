d3eb964da2efef4a1e22fa019c85a363c9e17247
contact9devs <contact9devs@gmail.com>
2024-02-02 10:59:06 +0000

    correction des erreurs

--- script/switchTheme.js
+++ script/switchTheme.js
@@ -1,16 +1,22 @@
-const switchButton = document.querySelector('.switch')
-theme = 0
+// const switchButton = document.querySelector('.switch')
+// let theme = 0
 
-switchButton.addEventListener('click', ()=>{
-  if(theme === 0){
-    document.documentElement.style.setProperty('--text-color','#ffff')
-    document.documentElement.style.setProperty('--background','black')
-    switchButton.innerText = "Light Mode" 
-    theme++
-  }else{
-    document.documentElement.style.setProperty('--text-color','#4A5568')
-    document.documentElement.style.setProperty('--background','#ffff')
-    switchButton.innerText = "Dark Mode"
-    theme--
-  }
-})
+// switchButton.addEventListener('click', ()=>{
+//   if(theme === 0){
+//     document.documentElement.style.setProperty('--text-color','#ffff')
+//     document.documentElement.style.setProperty('--background','black')
+//     document.documentElement.style.setProperty('--card-text-color','#ffff')
+//     document.documentElement.style.setProperty('--background-form','#29212180')
+//     document.documentElement.style.setProperty('--box-shadow-nav', '#ffff')
+//     switchButton.innerText = "Light Mode" 
+//     theme++
+//   }else{
+//     document.documentElement.style.setProperty('--text-color','#4A5568')
+//     document.documentElement.style.setProperty('--background','#ffff')
+//     document.documentElement.style.setProperty('--card-text-color','#16205F')
+//     document.documentElement.style.setProperty('--background-form','#F9F9FF')
+//     document.documentElement.style.setProperty('--box-shadow-nav', 'rgba(0, 0, 0, 0.15)')
+//     switchButton.innerText = "Dark Mode"
+//     theme--
+//   }
+// })
